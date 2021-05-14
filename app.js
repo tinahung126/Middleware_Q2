7ede0535e55488d3a02b52171eb1e16013da183f
tinahung126 <tina126hung@gmail.com>
2021-05-14 16:14:59 +0800

    feat: fix JS standard

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
 // app.js
-const express = require('express');
-const app = express();
-const port = 3000;
-let originalUrl = '';
-let logMethod = '';
-let startTime = 0;
-let endTime = 0;
+const express = require('express')
+const app = express()
+const port = 3000
+let originalUrl = ''
+let logMethod = ''
+let startTime = 0
+let endTime = 0
 
-function logProcess(req, res, next) {
-  originalUrl = req.originalUrl;
-  logMethod = req.method;
-  startTime = new Date();
-  next();
+function logProcess (req, res, next) {
+  originalUrl = req.originalUrl
+  logMethod = req.method
+  startTime = new Date()
+  next()
 }
 
-function endPoint(req, res, next) {
-  endTime = new Date();
-  timeCount = (endTime - startTime).toLocaleString();
-  startTime = startTime.toLocaleString();
+function endPoint (req, res, next) {
+  endTime = new Date()
+  timeCount = (endTime - startTime).toLocaleString()
+  startTime = startTime.toLocaleString()
   console.log(
     `${startTime} | ${logMethod} from ${originalUrl} | total time: ${timeCount} ms`
-  );
+  )
 }
 
 app.get(
   '/',
   logProcess,
   (req, res, next) => {
-    res.send('列出全部 Todo');
-    next();
+    res.send('列出全部 Todo')
+    next()
   },
   endPoint
-);
+)
 
 app.get(
   '/new',
   logProcess,
   (req, res, next) => {
-    res.send('新增 Todo 頁面');
-    next();
+    res.send('新增 Todo 頁面')
+    next()
   },
   endPoint
-);
+)
 
 app.get(
   '/:id',
   logProcess,
   (req, res, next) => {
-    res.send('顯示一筆 Todo');
-    next();
+    res.send('顯示一筆 Todo')
+    next()
   },
   endPoint
-);
+)
 
 app.post(
   '/',
   logProcess,
   (req, res, next) => {
-    res.send('新增一筆  Todo');
-    next();
+    res.send('新增一筆  Todo')
+    next()
   },
   endPoint
-);
+)
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+  console.log(`App running on port ${port}`)
+})
